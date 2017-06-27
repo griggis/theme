d7b0fc00beb2cca5cc4ee117dd686892145352aa
vahegrig <vahegrigori@gmail.com>
2017-06-27 13:18:24 +0000

    New components

--- src/config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  api: {
-    restBase: 'http://griggi.org',
-    wpPath: '/wp-json/v2'
-  }
-}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { AppContainer } from 'react-hot-loader'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import RootContainer from './containers/'
+import Slider from './components/slider'
 
 const rootEl = document.getElementById('builder-root')
 const render = Component =>
@@ -12,5 +12,5 @@ const render = Component =>
     rootEl
   )
 
-render(RootContainer)
-if (module.hot) module.hot.accept('./containers', () => render(RootContainer))
+render(Slider)
+if (module.hot) module.hot.accept('./containers', () => render(Slider))
